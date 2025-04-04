8432576f92d30152110c266cf8270b0e2bec959e
buknat-WebApp <buknatwebapp@gmail.com>
2025-04-04 11:51:40 +0800

    Remove the Logo in notification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -114,10 +114,6 @@
                                 <li class="mb-2">
                                     <a class="dropdown-item border-radius-md bg-gradient-faded-info" href="javascript:;">
                                         <div class="d-flex py-1">
-                                            <div class="my-auto">
-                                                <!-- Update image path to use asset() helper -->
-                                                <img src="{{ asset('assets/assets/img/logo.png') }}" class="avatar avatar-sm  me-3 ">
-                                            </div>
                                                     <div class="d-flex flex-column justify-content-center">
                                                         <h6 class="text-sm font-weight-normal mb-1">
                                                             <span class="font-weight-bold">{{$notification->data['name']}} {{$notification->data['data']}}</span>
